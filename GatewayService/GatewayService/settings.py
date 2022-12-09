81096250136542c3b571f7178bf60f59dbcd8780
sohaibssb <sohayb.salem2012@gmail.com>
2022-12-09 03:03:57 +0300

    Update3

--- GatewayService/GatewayService/settings.py
+++ GatewayService/GatewayService/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'GatewayServiceS'
+    'GatewayServiceS',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
